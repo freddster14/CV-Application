688b845381871f897b592202075f6c8e497631f0
Zeronicc <zeronic4465@gmail.com>
2024-09-18 17:53:41 -0500

    style: implemented title

---
+++ src/components/Intro.jsx
@@ -0,0 +1,7 @@
+import '../styles/Intro.css'
+
+export default function Intro() {
+    return (
+        <h1 className="app-name">CV Builder</h1>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './components/App.jsx'
+import App from './components/App'
+import  Intro  from './components/Intro'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Intro/>
     <App />
   </StrictMode>,
 )
